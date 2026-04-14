60ef9564a4d3b62ac6829232fae365f76676e0b6
Pedro <pedrodelmonico@hotmail.com>
2026-04-14 18:51:02 -0300

    chore: init

---
+++ src/main/java/br/com/pedromaia/gestao_vagas/modules/candidates/controllers/CandidateContrller.java
@@ -0,0 +1,20 @@
+package br.com.pedromaia.gestao_vagas.modules.candidates.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pedromaia.gestao_vagas.modules.candidates.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidates")
+public class CandidateContrller {
+
+  @PostMapping("/")
+  public void create(@RequestBody CandidateEntity candidate) {
+    System.out.println(candidate);
+  }
+
+
+}
