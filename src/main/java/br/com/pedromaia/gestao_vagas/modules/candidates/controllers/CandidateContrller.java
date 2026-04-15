bf9a3f6bc3bf36561fb855e5aa959f54af1f7b6c
Pedro <pedrodelmonico@hotmail.com>
2026-04-15 20:52:10 -0300

    feat

--- src/main/java/br/com/pedromaia/gestao_vagas/modules/candidates/controllers/CandidateContrller.java
+++ src/main/java/br/com/pedromaia/gestao_vagas/modules/candidates/controllers/CandidateContrller.java
@@ -1,5 +1,6 @@
 package br.com.pedromaia.gestao_vagas.modules.candidates.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import br.com.pedromaia.gestao_vagas.modules.candidates.CandidateEntity;
 public class CandidateContrller {
 
   @PostMapping("/")
-  public void create(@RequestBody CandidateEntity candidate) {
+  public void create(@Valid @RequestBody CandidateEntity candidate) {
     System.out.println(candidate);
   }
 
